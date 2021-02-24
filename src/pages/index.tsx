861d0f728a756f233c8f90217bd32d5b90e05429
Juliano Tavares <juliano.tavaress@gmail.com>
2021-02-23 21:00:21 -0300

    added countdown

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { CompletedChallenges } from "../components/CompletedChallenges";
+import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
+
+import Head from 'next/head'
 
 import styles from '../styles/pages/Home.module.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
       
         <ExperienceBar />
 
@@ -15,6 +21,7 @@ export default function Home() {
           <div>
             <Profile />
             <CompletedChallenges />
+            <Countdown />
           </div>
           <div>
 
