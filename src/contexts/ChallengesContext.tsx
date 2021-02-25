6aaccfb7a95a3e7d0eefc17c834ae1e7c8d33bf4
Juliano Tavares <juliano.tavaress@gmail.com>
2021-02-25 18:17:15 -0300

    added desafios

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,11 +1,20 @@
 import { createContext, useState, ReactNode } from 'react'
+import challenges from '../../challenges.json';
+
+interface Challenge {
+  type: 'body' | 'eye';
+  description: string;
+  amount: number;
+}
 
 interface ChallengesContextData {
   level: number;
   currentExperience: number;
   challengesCompleted: number;
+  activeChallenge: Challenge;
   levelUp: () => void;
   startNewChallenge: () => void;
+  resetChallenge: () => void;
 }
 
 interface ChallengesProviderProps {
@@ -19,13 +28,23 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
   const [currentExperience, setCurrentExperience] = useState(0)
   const [challengesCompleted, setChallengesCompleted] = useState(0)
   
+  const [activeChallenge, setActiveChallenge] = useState(null)
+  
   function levelUp() {
     setLevel(level + 1);
   }
 
   function startNewChallenge() {
+    const randomChallengeIndex = Math.floor(Math.random() * challenges.length)
+    const challenge = challenges[randomChallengeIndex];
 
+    setActiveChallenge(challenge);
   }
+
+  function resetChallenge(){
+    setActiveChallenge(null);
+  }
+
   return(
   <ChallengesContext.Provider 
     value={{ 
@@ -34,6 +53,8 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
       challengesCompleted, 
       levelUp,
       startNewChallenge,
+      activeChallenge,
+      resetChallenge,
       }}
       >
     { children }
