0ac2c6c58f2625851c62247d283b687d2d174c49
Juliano Tavares <juliano.tavaress@gmail.com>
2021-02-26 20:02:37 -0300

    finalizado aplicacao

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -15,7 +15,6 @@ export function Countdown() {
   const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('')
   const [secondsLeft, secondsRight] = String(seconds).padStart(2, '0').split('')
 
-
   return (
     <div>
       <div className={styles.countdownContainer}>
@@ -64,13 +63,6 @@ export function Countdown() {
         </>
       )
     }
-
-
-
-
-
-
-
     </div>
   )
 }
